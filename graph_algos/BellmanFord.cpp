92b1c7864741d1dd23dc1424183f56be94ffd2f2
manglakunal2701 <manglakunal2701@gmail.com>
2023-01-18 13:39:32 +0530

    eight-day

---
+++ graph_algos/BellmanFord.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cout<<"Enter the number of nodes"<<endl;
+    cin>>n;
+    cout<<"now enter the number of edges "<<endl;
+    cin>>m;
+    vector<pair<int,pair<int,int>>>arr;
+    for(int i=0;i<m;i++){
+        int x,y,wt;
+        cin>>x>>y>>wt;
+        arr.push_back(make_pair(wt,make_pair(x,y)));
+    }
+    cout<<"graph you entered is:"<<endl;
+     for(int i=0;i<arr.size();i++){
+       cout<<arr[i].second.first<<" "<<arr[i].second.second<<" "<<arr[i].first<<endl;
+   }
+   int sum=0;
+   vector<int>dist(n,INT_MAX);
+   dist[0]=0;
+   for(int i=0;i<n-1;i++){
+        for(auto it:arr){
+           int wt= it.first;
+           int x=it.second.first;
+           int y=it.second.second;
+           if(dist[x]+wt< dist[y]){
+               dist[y]=dist[x]+wt;
+           }
+       }
+   }
+   int flag=0;
+    for(int it=0;it<arr.size();it++){
+       int wt= arr[it].first;
+       int x=arr[it].second.first;
+       int y=arr[it].second.second;
+       if(dist[x]+wt< dist[y]){
+          cout<<"Negative cycle"<<endl;
+           flag=-1;
+           break;
+        }
+   }
+   if(flag==0){
+        for(int i=0;i<dist.size();i++){
+            cout<<dist[i]<<" ";
+        }
+        cout<<"Shortest path is:"<<sum<<endl;
+   }
+}
---
+++ graph_algos/dijshktra.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printParent(vector<int>&parent,int u){
+    cout<<u<<" ";
+    if(parent[u]>=0){
+        printParent(parent,parent[u]);
+    }
+}
+int  main(){
+   cout<<"enter the nodes of graph"<<endl;
+   int n,m;
+   cin>>n;
+   cout<<"Enter the number of edges of graph"<<endl;
+   cin>>m;
+   vector<pair<int,int>> arr[n+1];
+   for(int i=0;i<m;i++){
+       int u,v,wt;
+       cin>>u;
+       cin>>v;
+       cin>>wt;
+       arr[u].push_back({v,wt});
+       arr[v].push_back({u,wt});
+   }
+   for(int i=1;i<n+1;i++){
+       cout<<i<<"->";
+       for(auto it=arr[i].begin();it!=arr[i].end();it++){
+           cout<<"{"<<it->first<<" "<<it->second<<"}";
+       }cout<<endl;
+   }
+  // shortestPath(1,n+1,arr);
+   int src;
+   cout<<"Enter the source node"<<endl;
+   cin>>src;
+   priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+    vector<int>distArray(n+1,INT_MAX);
+     vector<int> parent(n+1,0);
+     parent[src]=-1;
+    distArray[src]=0;
+    pq.push(make_pair(0,src));
+    while(!pq.empty()){
+        int dist=pq.top().first;
+        int prev=pq.top().second;
+        pq.pop();
+        vector<pair<int,int>>::iterator it;
+        for(it=arr[prev].begin();it!=arr[prev].end();it++){
+            if(distArray[prev]+ it->second < distArray[it->first]){
+               distArray[it->first]=distArray[prev]+ it->second ;
+               pq.push(make_pair(distArray[it->first],it->first));
+               parent[it->first]=prev;
+            }
+        }
+    }
+    for(int i=1;i<n+1;i++){
+        cout<<distArray[i]<<" ";
+    }
+    // C:\Users\Kunal Mangla\Desktop\DSA_algorithms\GraphQuestions\DijkstraAlgoShortestPathUndirected.cpp
+    cout<<endl<<"the shortest distance of each vertex from source node is:"<<endl;
+    for(int i=1;i<parent.size();i++){
+        printParent(parent,i);
+         if (distArray[i] == INT_MAX)
+            cout << ": INF" << endl;
+        else
+            cout << ": " << distArray[i] << endl;    
+    }
+
+ }
+   
